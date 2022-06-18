1bd222f40a7506a3e5bc65104f5809057ee476fd
BonBon <bonbon.chen@basebit.ai>
2022-06-18 22:06:43 +0800

    init

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: "Vue.js设计与实现",
+  description: "Vue.js设计与实现读后感",
+  themeConfig: {
+    sidebar: [
+      {
+        items: [
+          { text: "权衡的艺术", link: "/" },
+          { text: "框架设计的核心要素", link: "/chapter2" },
+        ],
+      },
+    ],
+  },
+};
